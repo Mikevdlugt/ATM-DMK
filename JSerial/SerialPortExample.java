eac9ddd3c984e9fc02aae09df592d7e27062af31
MikeTrike <mikevdlugt@hotmail.com>
2024-04-19 11:14:48 +0200

    added SerialPortExample

---
+++ JSerial/SerialPortExample.java
@@ -0,0 +1,16 @@
+package HobbyStuff.JSerial;
+
+// import HobbyStuff.Swinging.Gui;
+
+public class SerialPortExample {
+
+    public static void main(String[] args) {
+        readData dataReader = new readData(115200, 4);
+        //implement function that reads from first ' to second ' for id
+        while (true) {
+            if(dataReader.checkForData()) {
+                System.out.println(dataReader.getData());
+            }
+        }
+    }
+}
